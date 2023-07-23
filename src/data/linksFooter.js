e95dbb7493b7acccb519b806cc08e89e5f529587
satoshi <n_krosh@mail.ru>
2023-07-24 02:54:13 +0300

    complite all layout

---
+++ src/data/linksFooter.js
@@ -0,0 +1,37 @@
+export const linksFooterMenu = [
+    {
+        url: "#",
+        name: "about",
+    },
+    {
+        url: "#",
+        name: "blog",
+    },
+    {
+        url: "#",
+        name: "shop",
+    },
+    {
+        url: "#",
+        name: "contact us",
+    },
+]
+
+export const linksFooterOther = [
+    {
+        url: "#",
+        name: "Styleguide",
+    },
+    {
+        url: "#",
+        name: "Changelog",
+    },
+    {
+        url: "#",
+        name: "licenses",
+    },
+    {
+        url: "#",
+        name: "Team",
+    },
+]
--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,5 +1,28 @@
+import { linksFooterMenu, linksFooterOther } from "../data/linksFooter"
+import Container from "../layout/Container"
+import Description from "../UI/Description"
+import FooterLinks from "../UI/FooterLinks"
+import LogoIcon from "../UI/LogoIcon"
+import style from "./Footer.module.scss"
+
 const Footer = () => {
-    return <div>Footer</div>
+    return (
+    <footer className={style.footer}>
+        <Container>
+            <div className={style.wrapper}>
+                <div className={style.shortInformation}>
+                <span width="170px" height="48px"><LogoIcon /></span>
+                <Description mt={35} fsize={0.9375} boxWidth={289}>
+                    Lorem ipsum dolor amet, consectetur adipiscing elit. Eget nisl nunc quam ac sed turpis volutpat. Cursus sed massa non .
+                </Description>
+            </div>
+                <FooterLinks name="Menu Items" links={linksFooterMenu}/>
+                <FooterLinks name="Other Pages" links={linksFooterOther}/>
+            </div>
+            
+        </Container>
+    </footer>
+    )
 }
 
 export default Footer
